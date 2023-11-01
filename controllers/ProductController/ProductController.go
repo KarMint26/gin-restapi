9abaf8a1c6b1584c63d3c059fff22f9a464ab3c5
Karel Trisnanto Utomo <kareltrisnanto26@gmail.com>
2023-11-01 21:02:40 +0700

    add: Final Commits Gin REST API

--- controllers/ProductController/ProductController.go
+++ controllers/ProductController/ProductController.go
@@ -2,9 +2,11 @@ package productcontroller
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/KarMint26/gin-restapi/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context) {
@@ -15,17 +17,61 @@ func Index(c *gin.Context) {
 }
 
 func Show(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
 
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "data tidak ditemukan"})
+			return
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{"product": product})
 }
 
 func Create(c *gin.Context) {
+	var product models.Product
 
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	models.DB.Create(&product)
+	c.JSON(http.StatusOK, gin.H{"product": product})
 }
 
 func Update(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
 
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Model(&product).Where("id = ?", id).Updates(&product).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "tidak dapat mengupdate product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil diperbarui"})
 }
 
 func Delete(c *gin.Context) {
+	var product models.Product
+	id, _ := strconv.Atoi(c.Param("id"))
 
+	if models.DB.Delete(&product, id).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "tidak dapat menghapus product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil dihapus"})
 }
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r.GET("/api/product/:id", productcontroller.Show)
 	r.POST("/api/product", productcontroller.Create)
 	r.PUT("/api/product/:id", productcontroller.Update)
-	r.DELETE("/api/products/:id", productcontroller.Delete)
+	r.DELETE("/api/product/:id", productcontroller.Delete)
 
 	r.Run(":8001")
 }
